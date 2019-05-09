1d81c9b42c7bad8823b559ff8616078d5496c41e
Hannah Ward <Hannah.ward9001@gmail.com>
2019-05-09 12:00:38 +0100

    Add tests for config resolution

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # SecretsCache
 
-**TODO: Add description**
+A cache and config merger for AWS Secrets. Give it a secret name, and an OTP application
+name and it'll resolve the resultant configuration.
+
+For example, let's say you've configured `:my_app` as such
+
+```elixir
+config :my_app,
+    best_yuru: :yui
+```
+
+And you have in AWS a secret named `my_secret`, that looks like
+```elixir
+{"database": {"username": "user", "password": "heh"}}
+```
+
+SecretsCache can resolve this as follows:
+
+```elixir
+SecretsCache.get_config(:my_app, "my_secret")
+[database: [username: "user", password: "heh", best_yuru: :yui]]
+```
 
 ## Installation
 
--- test/secrets_cache_test.exs
+++ test/secrets_cache_test.exs
@@ -1,8 +1,33 @@
 defmodule SecretsCacheTest do
   use ExUnit.Case
-  doctest SecretsCache
 
-  test "greets the world" do
-    assert SecretsCache.hello() == :world
+  describe "Config merge" do
+    test "works for flat keys" do
+      secret = %{
+        key: "value"
+      }
+      |> Jason.encode!()
+
+      {:ok, true} = Cachex.put(:aws_secrets_cache, "test", secret)
+      Application.put_env(:my_app, :existing_key, "yui")
+
+      result = SecretsCache.get_config(:my_app, "test")
+      assert result[:existing_key] == "yui"
+      assert result[:key] == "value"
+    end
+
+    test "works for embedded keys" do
+      secret = %{
+        "key.embedded": "value"
+      } 
+      |> Jason.encode!()
+
+      {:ok, true} = Cachex.put(:aws_secrets_cache, "test", secret)
+      Application.put_env(:my_app, :existing_key, "yui")
+
+      result = SecretsCache.get_config(:my_app, "test")
+      assert result[:existing_key] == "yui"
+      assert result[:key][:embedded] == "value"
+    end
   end
 end
